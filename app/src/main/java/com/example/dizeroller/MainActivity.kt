e371c3014fddb52907f26440157ff5bc78af647e
Matias Reyes <matirey1@gmail.com>
2024-04-17 09:07:31 -0400

    ADD: tools

--- app/src/main/java/com/example/dizeroller/MainActivity.kt
+++ app/src/main/java/com/example/dizeroller/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.dizeroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage : ImageView
+    private lateinit var rollButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        rollButton = findViewById(R.id.roll_button)
+        "Let's Roll".also { rollButton.text = it }
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        val randomInt : Int = Random.nextInt(6)+1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
